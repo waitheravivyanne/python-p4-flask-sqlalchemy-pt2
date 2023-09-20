dc41dc83182958a55a9457a4fb6d261306439949
vivian waithera <vivyannekori@gmail.com>
2023-09-20 20:56:01 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -5,7 +5,7 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy
 
 class Owner(db.Model):
     __tablename__ = 'owners'
@@ -22,8 +22,8 @@ class Pet(db.Model):
     __tablename__ = 'pets'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    name = db.Column(db.String(255))
+    species = db.Column(db.String(255))
 
     owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
 
--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,29 @@ from faker import Faker
 
 from app import app
 from models import db, Owner, Pet
+
+db.init_app(app)
+
+fake = Faker()
+# if not hasattr(app, 'extensions') or 'sqlalchemy' not in app.extensions:
+
+with app.app_context():
+
+    Pet.query.delete()
+    Owner.query.delete()
+
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    db.session.add_all(owners)
+
+    pets = []
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fake.first_name(), species=rc(species), owner=rc(owners))
+        pets.append(pet)
+
+    db.session.add_all(pets)
+    db.session.commit()
