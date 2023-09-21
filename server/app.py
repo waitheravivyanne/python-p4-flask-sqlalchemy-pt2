bb48558cd04ec0114c48084b33d27935f457d8cc
vivian waithera <vivyannekori@gmail.com>
2023-09-21 11:05:54 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,69 +1,67 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db, Pet, Owner
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///server/instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+db.init_app(app)
 
 migrate = Migrate(app, db)
 
-db.init_app(app)
 
 @app.route('/')
-def index():
-    response = make_response(
-        '<h1>Welcome to the pet/owner directory!</h1>',
-        200
-    )
-    return response
+def home():
+    return '<h1>Zoo app</h1>'
 
-@app.route('/pets/<int:id>')
+@app.route('/pet/<int:id>')
 def pet_by_id(id):
-    pet = Pet.query.filter(Pet.id == id).first()
+    pet = Pet.query.get(id)
+    if pet:
+        response_body = f'''
+            <h1>{pet.name}</h1>
+            <h2>Species: {pet.species}</h2>
+            <h3>Owner: {pet.owner.name}</h3>
+        '''
+        status_code = 200
+    else:
+        response_body = '<h1>Pet not found</h1>'
+        status_code = 404
 
-    if not pet:
-        response_body = '<h1>404 pet not found</h1>'
-        response = make_response(response_body, 404)
-        return response
-
-    response_body = f'''
-        <h1>Information for {pet.name}</h1>
-        <h2>Pet Species is {pet.species}</h2>
-        <h2>Pet Owner is {pet.owner.name}</h2>
-    '''
-
-    response = make_response(response_body, 200)
-
-    return response
+    headers = {}
+    return make_response(response_body, status_code, headers)
 
 @app.route('/owner/<int:id>')
 def owner_by_id(id):
-    owner = Owner.query.filter(Owner.id == id).first()
-
-    if not owner:
-        response_body = '<h1>404 owner not found</h1>'
-        response = make_response(response_body, 404)
-        return response
-
-    response_body = f'<h1>Information for {owner.name}</h1>'
-
-    pets = [pet for pet in owner.pets]
-
-    if not pets:
-        response_body += f'<h2>Has no pets at this time.</h2>'
-
-    else:
+    owner = Owner.query.get(id)
+    if owner:
+        pets = owner.pets
+        response_body = f'''
+            <h1>{owner.name}</h1>
+            <h2>Phone: {owner.phone}</h2>
+            <h3>Pets:</h3>
+            <ul>
+        '''
         for pet in pets:
-            response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
+            response_body += f'<li>{pet.name} - {pet.species}</li>'
+        response_body += '</ul>'
+        status_code = 200
+    else:
+        response_body = '<h1>Owner not found</h1>'
+        status_code = 404
 
-    response = make_response(response_body, 200)
 
-    return response
+    headers = {}
+    return make_response(response_body, status_code, headers)
 
+# 
 
 if __name__ == '__main__':
+
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,22 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+db = SQLAlchemy()
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db = SQLAlchemy
-
-class Owner(db.Model):
-    __tablename__ = 'owners'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-
-    pets = db.relationship('Pet', backref='owner')
-
-    def __repr__(self):
-        return f'<Pet Owner {self.name}>'
 
 class Pet(db.Model):
     __tablename__ = 'pets'
@@ -29,3 +18,16 @@ class Pet(db.Model):
 
     def __repr__(self):
         return f'<Pet {self.name}, {self.species}>'
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(self):
+        return f'<Pet Owner {self.name}>'
+
+
